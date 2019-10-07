4e200daafa43a3146af5fa5b025e5e0a5c566795
Ankan Poddar <friendyankan@gmail.com>
2019-10-07 12:55:33 +0530

    Update backend and docker-compose file

--- backend/app.py
+++ backend/app.py
@@ -9,23 +9,33 @@
 # bills_post = posts.find_one({'author': 'Bill'})
 # print(bills_post)
 
-from flask import Flask, jsonify
-from pymongo import MongoClient
+from flask import Flask, jsonify, request
+# from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-client = MongoClient('mongodb://db:27017')
-db = client['pymongo_test']
+# client = MongoClient('mongodb://db:27017')
+# db = client['pymongo_test']
+#
+# word_list = []
+# words = db.words.find()
+# for word in words:
+#     word_list.append({
+#         'id': str(word['_id']),
+#         'value': word['value']
+#     })
+#
+#
+# @app.route('/api/v1.0/words', methods=['GET'])
+# def words():
+#     return jsonify({'words': word_list})
 
-word_list = []
-words = db.words.find()
-for word in words:
-    word_list.append({
-        'id': str(word['_id']),
-        'value': word['value']
-    })
 
-
-@app.route('/api/v1.0/words', methods=['GET'])
-def get_tasks():
-    return jsonify({'words': word_list})
+@app.route('/api/v1.0/submit_data', methods=['POST'])
+def get_data():
+    print(request.form)
+    # print(request.form['data'])
+    print(request.files)
+    return jsonify(request.form)
--- backend/manage.py
+++ backend/manage.py
@@ -1,27 +1,27 @@
 from flask_script import Manager
-from pymongo import MongoClient
+# from pymongo import MongoClient
 from app import app
 
 manager = Manager(app)
-client = MongoClient('mongodb://db:27017')
-db = client['pymongo_test']
+# client = MongoClient('mongodb://db:27017')
+# db = client['pymongo_test']
 
 
 @manager.command
 def runserver():
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
 
-@manager.command
-def loadwords(filename):
-    words = []
-    with open(filename, 'r') as words_file:
-        for line in words_file:
-            words.append({
-                'value': line.strip()
-            })
-    words_obj = db.words
-    words_obj.insert(words)
+# @manager.command
+# def loadwords(filename):
+#     words = []
+#     with open(filename, 'r') as words_file:
+#         for line in words_file:
+#             words.append({
+#                 'value': line.strip()
+#             })
+#     words_obj = db.words
+#     words_obj.insert(words)
 
 
 if __name__ == "__main__":
