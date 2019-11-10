c4c441c8897b7eca5e02584d6c17215c6ec06e3b
Akshat7321 <akshat7321.aj@gmail.com>
2019-11-10 19:48:46 +0530

    Added cancel button

--- frontend/src/components/recorder.js
+++ frontend/src/components/recorder.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default class RecordingAPI extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      recording: false
+      recording: false,
+      recordedChunks: []
     };
   }
 
@@ -14,15 +15,19 @@ export default class RecordingAPI extends React.Component {
       video: false
     };
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    this.recordedChunks = [];
+    this.setState({
+      recordedChunks: []
+    });
     this.mediaRecorder = new MediaRecorder(stream);
 
-    this.mediaRecorder.ondataavailable = (e) => {
+    this.mediaRecorder.ondataavailable = e => {
       if (e.data.size > 0) {
-        this.recordedChunks.push(e.data);
+        this.setState({
+          recordedChunks: [...this.state.recordedChunks, e.data]
+        });
       }
     };
-
+    //console.log("record start", this.state.recordedChunks);
     this.mediaRecorder.start(10);
     this.setState({ recording: true });
   }
@@ -30,36 +35,58 @@ export default class RecordingAPI extends React.Component {
   stopRecording(e) {
     this.mediaRecorder.stop();
 
-    const blob = new Blob(this.recordedChunks, {'type' : 'audio/wav'});
-    const player = this.refs['player_' + this.props.id];
+    const blob = new Blob(this.state.recordedChunks, { type: "audio/wav" });
+    const player = this.refs["player_" + this.props.id];
     var blobUrl = URL.createObjectURL(blob);
-
+    //console.log("record stop", this.state.recordedChunks);
     player.src = blobUrl;
 
-    this.props.onDataSubmit(blob);
+    this.props.onDataSubmit(blob, false);
 
     this.setState({ recording: false });
   }
 
   render() {
-    const {recording} = this.state;
+    const { recording } = this.state;
     return (
       <span className="recorder">
-        {!recording &&
+        {!recording && (
           <button
             className="waves-effect waves-light btn-floating red record"
-            id="record" onClick={this.recordAudio.bind(this)}>
-              <i class="material-icons white-text">mic</i>
+            id="record"
+            onClick={this.recordAudio.bind(this)}
+          >
+            <i class="material-icons white-text">mic</i>
           </button>
-        }
-        {recording &&
+        )}
+        {recording && (
           <button
             className="waves-effect waves-light btn-floating deep-orange lighten-5 stop"
-            id="stop" onClick={this.stopRecording.bind(this)}>
-              <i class="material-icons red-text">stop</i>Stop
+            id="stop"
+            onClick={this.stopRecording.bind(this)}
+          >
+            <i class="material-icons red-text">stop</i>Stop
           </button>
-        }
+        )}
         <audio ref={"player_" + this.props.id} controls />
+        <button
+          disabled={this.state.recordedChunks.length === 0}
+          onClick={() => {
+            const player = this.refs["player_" + this.props.id];
+            player.src = "";
+            this.setState(
+              {
+                recordedChunks: []
+              },
+              () => {
+                //console.log("cancel vala", this.state.recordedChunks);
+                this.props.onDataSubmit(null, true);
+              }
+            );
+          }}
+        >
+          Cancel
+        </button>
       </span>
     );
   }
