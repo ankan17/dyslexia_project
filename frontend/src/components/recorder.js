14a24f5efd80b30cc33e449ec3759a5a34824967
Ankan Poddar <friendyankan@gmail.com>
2019-11-11 10:14:53 +0530

    [CLEANUP] Streamline code for test.js and recorder.js

--- frontend/src/components/recorder.js
+++ frontend/src/components/recorder.js
@@ -5,7 +5,8 @@ export default class RecordingAPI extends React.Component {
     super(props);
     this.state = {
       recording: false,
-      recordedChunks: []
+      recordedChunks: [],
+      dataUrl: ""
     };
   }
 
@@ -15,20 +16,15 @@ export default class RecordingAPI extends React.Component {
       video: false
     };
     const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    this.setState({
-      recordedChunks: []
-    });
     this.mediaRecorder = new MediaRecorder(stream);
 
     this.mediaRecorder.ondataavailable = e => {
       if (e.data.size > 0) {
-        this.setState({
-          recordedChunks: [...this.state.recordedChunks, e.data]
-        });
+        this.setState({ recordedChunks: [...this.state.recordedChunks, e.data] });
       }
     };
-    //console.log("record start", this.state.recordedChunks);
-    this.mediaRecorder.start(10);
+
+    this.mediaRecorder.start(5);
     this.setState({ recording: true });
   }
 
@@ -36,14 +32,12 @@ export default class RecordingAPI extends React.Component {
     this.mediaRecorder.stop();
 
     const blob = new Blob(this.state.recordedChunks, { type: "audio/wav" });
-    const player = this.refs["player_" + this.props.id];
     var blobUrl = URL.createObjectURL(blob);
-    //console.log("record stop", this.state.recordedChunks);
-    player.src = blobUrl;
+    var context = this;
 
-    this.props.onDataSubmit(blob, false);
-
-    this.setState({ recording: false });
+    this.setState({ dataUrl: blobUrl, recording: false }, () => {
+      context.props.onDataSubmit(blob);
+    });
   }
 
   render() {
@@ -52,41 +46,34 @@ export default class RecordingAPI extends React.Component {
       <span className="recorder">
         {!recording && (
           <button
-            className="waves-effect waves-light btn-floating red record"
-            id="record"
-            onClick={this.recordAudio.bind(this)}
+            className="waves-effect waves-light btn-floating deep-orange record"
+            id="record" onClick={this.recordAudio.bind(this)}
           >
-            <i class="material-icons white-text">mic</i>
+            <i className="material-icons white-text">mic</i>
           </button>
         )}
         {recording && (
           <button
-            className="waves-effect waves-light btn-floating deep-orange lighten-5 stop"
-            id="stop"
-            onClick={this.stopRecording.bind(this)}
+            className="waves-effect waves-light btn-floating red lighten-5 stop"
+            id="stop" onClick={this.stopRecording.bind(this)}
           >
-            <i class="material-icons red-text">stop</i>Stop
+            <i className="material-icons red-text">stop</i>Stop
           </button>
         )}
-        <audio ref={"player_" + this.props.id} controls />
-        <button
-          disabled={this.state.recordedChunks.length === 0}
-          onClick={() => {
-            const player = this.refs["player_" + this.props.id];
-            player.src = "";
-            this.setState(
-              {
-                recordedChunks: []
-              },
-              () => {
-                //console.log("cancel vala", this.state.recordedChunks);
-                this.props.onDataSubmit(null, true);
-              }
-            );
-          }}
-        >
-          Cancel
-        </button>
+        <audio src={this.state.dataUrl} controls />
+        { this.state.dataUrl !== "" &&
+          <button
+            className="waves-light btn-flat ml-1"
+            onClick={() => {
+              var context = this.props;
+              this.setState({ dataUrl: "", recordedChunks: [] }, () => {
+                context.onDataRemove();
+              });
+            }}
+          >
+            <i className="material-icons red-text" style={{ "fontSize": "21px", }}>delete</i>
+          </button>
+        }
       </span>
     );
   }
--- frontend/src/components/test.js
+++ frontend/src/components/test.js
@@ -11,7 +11,8 @@ export default class Test extends Component {
       words: [],
       audios: [],
       completed: [],
-      name: ""
+      name: "",
+      error: false
     };
   }
 
@@ -30,43 +31,54 @@ export default class Test extends Component {
       .then(response => {
         this.setState({
           completed: [...response.data.completed],
-          name: `${response.data.first_name} ${response.data.last_name}`
-        });
-      });
+          name: `${response.data.first_name}`
+        })
+      })
+      .catch(e => {
+        // Replace this part by routing to 404 page
+        this.setState({ error: e.message });
+      })
   }
 
-  addAudio(audio, index, id, cancel) {
+  addAudio(audio, index, id) {
     const new_audio_object = {
       blob: audio,
       id: id
     };
     const { audios } = this.state;
-    if (!cancel) audios[index] = new_audio_object;
-    else audios[index] = null;
+    audios[index] = new_audio_object;
     this.setState({ audios });
-    console.log("audio vaala array", audios);
+  }
+
+  removeAudio(index) {
+    const { audios } = this.state;
+    audios[index] = null;
+    this.setState({ audios });
   }
 
   submitData() {
     const { words, audios } = this.state;
+    const { match: { params } } = this.props;
     var data = [];
     for (var i = 0; i < words.length; i++) {
-      data.push({
-        word: words[i].value,
-        file: audios[i].blob
-      });
+      if (audios[i]) {
+        data.push({
+          id: words[i].id,
+          word: words[i].value,
+          file: audios[i].blob
+        });
+      }
     }
 
     var formData = new FormData();
-    var filename = new Date().toISOString();
 
     data.forEach((item, i) => {
-      formData.append(`${filename}-${i}`, item.file);
+      formData.append(`${item.id}_${item.word}.wav`, item.file);
     });
 
     axios({
       method: "post",
-      url: "http://localhost:8000/api/v1.0/submit_data",
+      url: `http://localhost:8000/api/v1.0/submit_data?id=${params.id}`,
       data: formData,
       config: {
         headers: {
@@ -79,79 +91,58 @@ export default class Test extends Component {
     });
   }
 
-  audiosIsNull() {
-    for (var j = 0; j < this.state.audios.length; j++) {
-      if (this.state.audios[j] != null) return false;
-    }
-    return true;
-  }
-
-  deleteAudio(x, num) {
-    const { audios } = this.state;
-    audios[num] = x;
-    this.setState({ audios });
-  }
-
   render() {
-    const { words, audios, completed, name } = this.state;
-    return (
-      <div className="container">
-        <h3 className="center-align">Welcome, {name}</h3>
-        <div className="pt-2 progress-bar right-align">
-          <div className="progress ">
-            <div
-              className="determinate"
-              style={{ width: `${(completed.length / words.length) * 100}%` }}
-            ></div>
+    const { words, audios, completed, name, error } = this.state;
+    if (!error) {
+      return (
+        <div className="container">
+          <h3 className="center-align">Welcome, { name }</h3>
+          <div className="pt-2 progress-bar right-align">
+            <div className="progress ">
+              <div
+                className="determinate"
+                style={{width: `${completed.length/words.length*100}%`}}>
+              </div>
+            </div>
+            <span className="progress-label">{completed.length}/{words.length}</span>
           </div>
-          <span className="progress-label">
-            {completed.length}/{words.length}
-          </span>
-        </div>
 
-        <div className="mt-2">
-          {words.map((word, index) => {
-            return completed.includes(word.id) ? (
-              <div key={word.id} id={word.id} className="row recorder">
-                <span
-                  className="recorder-word col s2 offset-s2"
-                  style={{ fontSize: "18px" }}
-                >
-                  {`${index + 1}. ${word.value}`}
-                </span>
-                <span className="green-text" style={{ fontSize: "16px" }}>
-                  Completed
-                </span>
-              </div>
-            ) : (
-              <div key={word.id} id={word.id} className="row">
-                <span
-                  className="recorder-word col s2 offset-s2"
-                  style={{ fontSize: "18px" }}
-                >
-                  {`${index + 1}. ${word.value}`}
-                </span>
-                <RecordingAPI
-                  id={index}
-                  onDataSubmit={(audio, cancel) =>
-                    this.addAudio(audio, index, word.id, cancel)
-                  }
-                  //kuchbhi={val => this.deleteAudio(val, index)}
-                />
-              </div>
-            );
-          })}
+          <div className="mt-2">
+            { words.map((word, index) => {
+              return completed.includes(word.id) ?
+                <div key={ word.id } id={ word.id } className="row recorder">
+                 <span className="recorder-word col s2 offset-s2" style={{ "fontSize": '18px' }}>
+                    { `${index+1}. ${word.value}` }
+                 </span>
+                 <span className="green-text" style={{ "fontSize": '16px' }}>Completed</span>
+                </div> :
+                <div key={ word.id } id={ word.id } className="row">
+                  <span className="recorder-word col s2 offset-s2" style={{ "fontSize": '18px' }}>
+                    { `${index+1}. ${word.value}` }
+                  </span>
+                  <RecordingAPI
+                    id={index}
+                    onDataSubmit={ audio => this.addAudio(audio, index, word.id) }
+                    onDataRemove={ () => this.removeAudio(index) }
+                  />
+                </div>
+              }
+            )}
+          </div>
+          <div className="row center-align mt-2">
+            <button
+              className="btn"
+              type="submit"
+              onClick={this.submitData.bind(this)}
+              disabled={audios.filter(x => x === null).length >= 4}
+            >
+              Submit
+            </button>
+          </div>
         </div>
-        <div className="row center-align mt-2">
-          <button
-            className="btn"
-            onClick={this.submitData.bind(this)}
-            disabled={this.audiosIsNull()}
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    );
+      );
+    } else {  // This part will also be removed
+      return <div>{ error }</div>
+    }
   }
 }
