7cbc29c461a5e1d1d97c6fa879d1dbe22c975ffd
Ankan Poddar <friendyankan@gmail.com>
2019-12-09 10:15:28 +0530

    Add separate command and method for recognizing words

--- backend/manage.py
+++ backend/manage.py
@@ -4,6 +4,8 @@ from flask_script import Manager
 from pymongo import MongoClient
 from app import app
 
+from utils import recognize_word, find_by_truncated_id
+
 
 manager = Manager(app)
 client = MongoClient('mongodb://db:27017')
@@ -26,6 +28,26 @@ def loadwords(filename, language):
             })
     words_obj = db.words
     words_obj.insert_many(words)
+
+
+@manager.command
+def recognizewords(subject=None, lang=None):
+    if not subject:
+        for sub in db.subjects.find():
+            if lang:
+                print(recognize_word(str(sub['_id'])[:8], lang))
+            else:
+                print(recognize_word(str(sub['_id'])[:8], 'ben'))
+                print(recognize_word(str(sub['_id'])[:8], 'eng'))
+    else:
+        sub = find_by_truncated_id(db.subjects, subject)
+        if sub:
+            if lang:
+                print(recognize_word(str(sub['_id'])[:8], lang))
+            else:
+                print(recognize_word(str(sub['_id'])[:8], 'ben'))
+                print(recognize_word(str(sub['_id'])[:8], 'eng'))
+
 
 
 if __name__ == "__main__":
--- backend/utils.py
+++ backend/utils.py
@@ -1,3 +1,9 @@
+import os
+import speech_recognition as sr
+
+from settings import UPLOAD_FOLDER
+
+
 def find_by_truncated_id(relation, id, key='_id'):
     for item in relation.find():
         if str(item[key])[:8] == id:
@@ -9,3 +15,29 @@ def find_by_truncated_id(relation, id, key='_id'):
 def delete_item(relation, id=None):
     if not id:
         relation.delete_many({})
+
+
+def recognize_word(id, lang):
+    r = sr.Recognizer()
+    lang_codes = {'ben': 'bn-IN', 'eng': 'en-IN'}
+
+    response = []
+
+    dir_path = os.path.join(UPLOAD_FOLDER, id, lang)
+    if os.path.exists(dir_path):
+        os.chdir(dir_path)
+        for filename in os.listdir(dir_path):
+            if filename.endswith(".wav"):
+                word_id, original_word = filename.split('_')
+                original_word = original_word[:-4]
+                with sr.AudioFile(filename) as source:
+                    audio = r.record(source)
+                recognized_word = r.recognize_google(audio, language=lang_codes[lang])
+
+                response.append({
+                    'id': word_id,
+                    'original': original_word,
+                    'recognized': recognized_word
+                })
+
+    return response
